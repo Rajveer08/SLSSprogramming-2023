69ee79c3e7c9028c8ca84868f159005bea1085b4
Rajveer08 <irajveer08@gmail.com>
2023-10-17 10:24:11 -0700

    quote generator

---
+++ quotegenerator.py
@@ -0,0 +1,36 @@
+# quote generator
+# author: rajveer
+# date 09 19 2023
+import random 
+quotes=["Love what you do to do great work.",
+"Remember the silence of friends, not the words of enemies.",
+"Life happens when plans change.",
+"Doubt limits tomorrow's possibilities.",
+"Rising after a fall is true glory.",
+"Life goes on.",
+"Be yourself; it's the greatest accomplishment.",
+"Courage counts more than success or failure.",
+"Triumph of evil requires inaction from good.",
+"Infinite universe, human stupidity.",
+"Create your future.",
+"Don't just exist; live.",
+"You decide your destiny.",
+"Change is life's constant.",
+"Purpose is happiness.",
+"Start where you are, change the ending.",
+"Happiness leads to success.",
+"Judge days by seeds planted, not harvest reaped.",
+"True wisdom is knowing you know nothing.",
+"Remember the silence of friends."]
+yes=1
+while yes == 1:
+    quoteAsk= input("do you want a quote ")
+    quote=random.choice(quotes)
+    if quoteAsk.capitalize()== "Yes":
+        print(quote)
+        
+    else:
+        print("oh your gonna say "+ quoteAsk + " i hate you")
+    
+        yes= yes+1
+         
