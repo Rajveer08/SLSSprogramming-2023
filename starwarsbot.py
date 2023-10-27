f724f4795bb2584cd7d82dae3deac6107bc0bd8c
Rajveer08 <irajveer08@gmail.com>
2023-10-27 11:39:18 -0700

    commited

--- starwarsbot.py
+++ starwarsbot.py
@@ -1,8 +1,7 @@
 darkSideq1=input("is your favorite color red? ")
 capeQ=input("do you like capes? ")
-if darkSideq1.capitalize()=="Yes" and capeQ.upper()=="Yes":
-    print("dark")
-elif darkSideq1.capitalize() and capeQ.upper()=="Yes":
+
+if darkSideq1.capitalize() == "Yes" or capeQ.capitalize() == "Yes":
     print("dark")
 else:
     print("light")
