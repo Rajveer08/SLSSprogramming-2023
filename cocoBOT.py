b0a65440551ece90571c4fc95dbe8a3a1d8c4656
Rajveer08 <irajveer08@gmail.com>
2023-10-27 12:50:17 -0700

    bubbletea

---
+++ cocoBOT.py
@@ -0,0 +1,15 @@
+#ask 5 users what their fav bubble tea place is
+#print out a summary of the data
+coco= 0
+chatime=0
+suntea=0
+xingfu=0
+bubblequeen=0
+#ask the user what their fave bbt place is
+print("whats your favorite bubble tea place?")
+fave_bbt= input().strip(",.?!").lower()
+#tallying/counting algo
+#options:coco chatime suntea xing fu bubble queen
+#if they say coco increse cpunter
+
+#print summary
