a3ddbf189799f3b6cafb25611c977e88b5c20b13
Rajveer08 <irajveer08@gmail.com>
2023-11-27 10:29:42 -0800

    finsihed the question

---
+++ starQ1.py
@@ -0,0 +1,11 @@
+#Rajveer
+#nov 27 2023
+
+
+
+def stars(star_num: int) -> float:
+	"""prints stars for how many numbers you put in
+        """
+	for number in range(star_num):
+		print("*")
+stars(10)
