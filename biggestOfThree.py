0cdb58d91aeb31a24f1706476bfb9d89fb01d071
Rajveer08 <irajveer08@gmail.com>
2023-11-27 10:57:43 -0800

    made the code more consice

--- biggestOfThree.py
+++ biggestOfThree.py
@@ -1,12 +1,12 @@
 #Rajveer
 #nov 27 2023
-def biggest_of_three(num_1: float, num_2: float,num_3: float):
+def biggest_of_three(num_1: float, num_2: float, num_3: float):
     """returns the biggest of three numbers"""
-    if num_1 > num_2 and num_3:
+    if num_1 > num_2 and num_1 > num_3:
         return num_1
-    if num_2 > num_1 and num_3:
+    elif num_2 > num_3:
         return num_2
-    if num_3 > num_2 and num_1:
+    else:
         return num_3
 
-print(f"{biggest_of_three(4,7,1)} is the biggest number")
+print(f"{biggest_of_three(132,113,111)} is the biggest number")
